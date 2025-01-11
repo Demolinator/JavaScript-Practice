a8085cdc5178e495711ea3f746abccab75775570
Talal Ahmed <ta234567801@gmail.com>
2025-01-12 02:22:31 +0500

    Created: JavaScript-to-do-list-manager

---
+++ JavaScript-To-Do-List-Manager/app.js
@@ -0,0 +1,84 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const tasks = [];
+
+function displayMenu() {
+  console.log("\nTo-Do List Manager:");
+  console.log("1. Add Task");
+  console.log("2. View Tasks");
+  console.log("3. Mark Task as Completed");
+  console.log("4. Delete Task");
+  console.log("5. Exit\n");
+}
+
+function handleinput(option) {
+  switch (option) {
+    case "1":
+      rl.question("Enter task description: ", (task) => {
+        tasks.push({ description: task, completed: false });
+        console.log(`Task added: "${task}"`);
+        mainMenu();
+      });
+      break;
+
+    case "2":
+      console.log("\nYour Tasks:");
+      if (tasks.length === 0) {
+        console.log("No tasks found!");
+      } else {
+        tasks.forEach((task, index) => {
+            const status = task.completed ? "[✔]" : "[ ]";
+            console.log(`${index + 1}. ${status} ${task.description}`);
+        });
+      }
+      mainMenu();
+      break;
+
+    case "3":
+        rl.question("Enter task number to mark as completed: ", (number) => {
+            const index = parseInt(number) - 1;
+            if (tasks[index]) {
+                tasks[index].completed = true;
+                console.log(`Task ${number} mark as completed!`);
+            } else {
+                console.log("Invalid task number");
+            }
+            mainMenu();
+        });
+        break;
+    
+    case "4":
+        rl.question("Enter task number to delete: ", (number) => {
+            const index = parseInt(number) - 1;
+            if (tasks[index]) {
+                const removed = tasks.splice(index, 1);
+                console.log(`Task "${removed[0].description}" deleted!`);
+            } else {
+                console.log("Invalid task number!");
+            }
+            mainMenu();
+        });
+        break;
+
+    case "5":
+        console.log("Goodbye!");
+        rl.close();
+        break;
+
+    default:
+        console.log("Invalid option! Please choose a number between 1 and 5.");
+        mainMenu();
+  }
+}
+
+function mainMenu () {
+    displayMenu();
+    rl.question("Chose an option: ", handleinput)
+}
+
+mainMenu()
